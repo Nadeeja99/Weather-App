4dd4601ceb945afc5d202478e1f0b98d9b78aa1b
Nadeeja99 <nadeejaperera123@gmail.com>
2020-08-09 00:01:18 +0530

    Simple Weather App

---
+++ app.js
@@ -0,0 +1,25 @@
+var button = document.querySelector('.button')
+var inputValue = document.querySelector('.inputValue')
+var name = document.querySelector('.name')
+var desc = document.querySelector('.desc')
+var temp = document.querySelector('.temp')
+
+button.addEventListener('click',function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&units=metric&appid=a2cffb13c4cf1f95e8b72a49c0af6b8e')
+.then(response => response.json())
+.then(data => {
+    var nameValue = data['name'];
+    var tempValue = data['main']['temp'];
+    var descValue = data['weather'][0]['description'];
+
+    name.innerHTML = nameValue;
+    temp.innerHTML = tempValue + "°C";
+    desc.innerHTML = descValue;
+})
+
+
+
+.catch(err => alert("Check the city name!!!"))
+})
+
+
